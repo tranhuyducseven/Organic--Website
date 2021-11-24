1ea9e8e66ad52f3f6056148c885410a09ca3b6af
rosen <you@example.com>
2021-11-24 22:13:20 +0700

    add view/footer.php

--- test.php
+++ test.php
@@ -156,7 +156,8 @@
         <!--end main-->
     </div>
     <!-- #############FOOTER######### -->
-    <?php require_once("./views/footer.php") ?>
+    
+
     <?php require_once("./views/canvas.php") ?>
     <script src="./assets/js/cart.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>    
